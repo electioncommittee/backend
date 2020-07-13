f590acc549183edd84d66a495a3fb0b49dd630dd
hyperbola <hyperbola.cs07@gmail.com>
2020-07-14 01:39:20 +0800

    clean code

--- src/api/candidates.js
+++ src/api/candidates.js
@@ -79,7 +79,6 @@ export default async function (req, res) {
     `;
     const args = [req.query.year];
     const rows = await query(sql, args);
-    const ret = rows.map(r => ({ name: r.name, id: r.id, no: r.no }));
-    if (ret.length === 0) res.sendStatus(404);
-    else res.send(ret);
+    if (rows.length === 0) res.sendStatus(404);
+    else res.send(rows);
 }
