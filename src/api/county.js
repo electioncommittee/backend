26b3de062b56bc412261030b45dc7e5a278d28fb
hyperbola <hyperbola.cs07@gmail.com>
2020-07-14 08:10:16 +0800

    Fix get-counties

--- src/api/county.js
+++ src/api/county.js
@@ -1,5 +1,7 @@
 import query from "../../lib/db";
 
+const MUNICIPALITY = ["台北市", "新北市", "桃園市", "台中市", "台南市", "高雄市"]
+
 export default async function (req, res) {
 
     if (req.query.year < 2009 || req.query.year > 2020) {
@@ -11,6 +13,8 @@ export default async function (req, res) {
     const ret = rows.filter(row => {
         if (row.id == 4 && req.query.year < 2014) return false;
         if (row.id == 5 && req.query.year > 2013) return false;
+        if (req.query.year == 2009) return !MUNICIPALITY.includes(row.name)
+        if (req.query.year == 2010) return MUNICIPALITY.includes(row.name)
         return true;
     });
     if (ret.length === 0) res.sendStatus(404);
